6f3d36e5a2c8a779d0210debfd7539a4b33033bb
rrsartpl <rrsartpl@gmail.com>
2021-01-19 16:20:33 +0100

    Nodemon setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ 'hello': 'there' })
+    res.send({ 'hello': 'rafa' })
 });
 
 const PORT = process.env.PORT || 5000;
