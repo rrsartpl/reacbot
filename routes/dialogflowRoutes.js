3530681d71eec8797b465f8bd900bd7f59762deb
rrsartpl <rrsartpl@gmail.com>
2021-01-19 19:01:59 +0100

    settting envrionment variables in windows

--- routes/dialogflowRoutes.js
+++ routes/dialogflowRoutes.js
@@ -1,3 +1,10 @@
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+
+const sessionClient = new dialogflow.SessionsClient();
+
+const sesssionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
 module.exports = app => {
 
     app.get('/', (req, res) => {
@@ -5,6 +12,35 @@ module.exports = app => {
     });
 
     app.post('/api/df_text_query', (req, res) => {
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: req.body.text,
+                    languageCode: config.dialogflowSessionLanguageCode,
+                },
+            },
+        };
+
+        sessionClient
+            .detectIntent(request)
+            .then(responses => {
+                console.log('Detected intent');
+                const result = responses[0].queryResult;
+                console.log(`  Query: ${result.queryText}`);
+                console.log(`  Response: ${result.fulfillmentText}`);
+                if (result.intent) {
+                    console.log(`  Intent: ${result.intent.displayName}`);
+                } else {
+                    console.log(`  No intent matched.`);
+                }
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+
+
         res.send({ 'do': 'text query' })
     });
 
