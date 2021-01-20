1bd0b874a3e03ee14e62eadc374778ecb1110a0f
rrsartpl <rrsartpl@gmail.com>
2021-01-20 16:43:55 +0100

    hello bug

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogflowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOgLE_PRIVATE_KEY
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
