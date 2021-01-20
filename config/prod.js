ec18b05c848a374b116f2b490006358efd8f4046
rrsartpl <rrsartpl@gmail.com>
2021-01-20 16:51:32 +0100

    hello hope

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,7 @@
-const { googleClientEmail } = require("./dev");
-
 module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
-    dialogflowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUGAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
-}
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+};
