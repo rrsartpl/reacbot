46f4a9284c4293ad7833a6f11b4d668d5c678934
rrsartpl <rrsartpl@gmail.com>
2021-01-20 16:05:44 +0100

    config local server env variables

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -4,6 +4,15 @@ const config = require('../config/keys');
 const sessionClient = new dialogflow.SessionsClient();
 const structjson = require('./structjson')
 
+const projectID = config.googleProjectID;
+
+const credentials = {
+    client_email: config.googleClientEmail,
+    private_key: config.googlePrivateKey
+}
+
+const sessionClient = new dialogflow.SessionsClient({ projectID, credentials })
+
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
 
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+const { googleClientEmail } = require("./dev");
+
+module.exports = {
+    googleProjectID: prcoess.env.GOOGLE_PROJECT_ID,
+    dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+    dialogflowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey: process.env.GOOgLE_PRIVATE_KEY
+}
