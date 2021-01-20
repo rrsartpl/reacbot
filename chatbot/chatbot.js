4762294de8b0c543c3b362cb06e32bc61785b3f4
rrsartpl <rrsartpl@gmail.com>
2021-01-20 14:11:32 +0100

    event query

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -2,6 +2,7 @@
 const dialogflow = require('dialogflow');
 const config = require('../config/keys');
 const sessionClient = new dialogflow.SessionsClient();
+const structjson = require('./structjson')
 
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
@@ -22,6 +23,28 @@ module.exports = {
             queryParams: {
                 data: parameters
             }
+        };
+
+        let responses = await sessionClient.detectIntent(request)
+        responses = await self.handleAction(responses);
+        return responses;
+    },
+
+    handleAction: function(responses) {
+        return responses;
+    },
+
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters),
+                    languageCode: config.dialogflowSessionLanguageCode
+                },
+            },
         };
 
         let responses = await sessionClient.detectIntent(request)
