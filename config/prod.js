a787f3b96af16ff77041fe8acbfd8e42c446c53b
rrsartpl <rrsartpl@gmail.com>
2021-01-20 16:35:22 +0100

    heroku problem 3

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 const { googleClientEmail } = require("./dev");
 
 module.exports = {
-    googleProjectID: prcoess.env.GOOGLE_PROJECT_ID,
+    googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogflowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
